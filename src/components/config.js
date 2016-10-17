151d179444fc76d9a6d6eab9936638a63ef55e18
Ratan Kadam <ratan@ratan-mac.local>
2016-10-16 23:01:35 -0700

    accordian

---
+++ src/components/config.js
@@ -0,0 +1,25 @@
+export default {
+  Acccordian:[
+    {
+      text:"acc1",
+      image: '/src/resource/intro-bg.jpg'
+    },
+    {
+      text:"acc2",
+      image: '/src/resource/intro-bg.jpg'
+    },
+    {
+      text:"acc3",
+      image: '/src/resource/intro-bg.jpg'
+    },
+    {
+      text:"acc3",
+      image: '/src/resource/intro-bg.jpg'
+    },
+    {
+      text:"acc3",
+      image: '/src/resource/intro-bg.jpg'
+    }
+  ],
+  navoptions:['Contact','Work Experience' , 'About']
+}
